2ed6f2a9668026610d1ac68fba5014f9217d69d8
Egor Nikolaev <egor@onlifegroup.com>
2011-09-20 20:42:47 +0400

    MicrodataJS Templater version 0.2

--- examples/microdataTemplater/microdataTemplater.js
+++ examples/microdataTemplater/microdataTemplater.js
@@ -1,10 +1,21 @@
 ﻿// @required(microdata)
-global = window;//TODO
+
+//closure
+;(function(global, MicrodataJSLib) {
 
 /** Темплейтер на основе HTML5 Microdata API
  * @param {Node} microdataItem Корневой DOM-элемент - Microdata Item
  * @param {Object|Array.<Object>} data Данные для вставки в microdataItem */
-function microdataItem_to_template(microdataItem, data) {
+global.microdataTemplate = new function() {
+	var thisObj = this;
+	
+/* PRIVATE */
+	var _documentFragment = document.createDocumentFragment();
+
+/* OPTIONS */
+	thisObj.options = thisObj.options || {};
+	
+/* PRIVATE | FUNCTIONS */
 	/**
 	 * Set the itemValue of an Element.
 	 * http://www.w3.org/TR/html5/microdata.html#values
@@ -12,13 +23,19 @@ function microdataItem_to_template(microdataItem, data) {
 	 * @param {Node} element The element to extract the itemValue for.
 	 * @param {string} value The itemValue of the element.
 	 * @param {string} itemprop Value of itemprop attribute
+	 * @param {boolean!} forse Forse reset text nodes
+	 * @return {string} value
 	 */
-	function setItemValue(element, value, itemprop) {
+	function setItemValue(element, value, itemprop, forse) {
 		var elementName = element.nodeName,
 			attrTo = "innerHTML",
 			attrFrom = attrTo,
-			tmplOptions = JSON.parse(element.getAttribute("data-tmpl-options")) || {};
+			tmplOptions = JSON.parse(element.getAttribute("data-tmpl-options")) || {},
+			templateElement = element.__templateElement__;
 
+		//temp
+		if(!templateElement)element.__templateElement__ = _documentFragment.appendChild(cloneElement(element));
+			
 		attrFrom = tmplOptions["source"] || attrFrom;
 		
 		if(tmplOptions.target)
@@ -33,10 +50,12 @@ function microdataItem_to_template(microdataItem, data) {
 			attrTo = "data";
 		else if (elementName === 'TIME' && element.getAttribute('datetime'))
 			attrTo = "dateTime";//TODO:: Check IE[7,6]
-		
+			
 		//Если в шаблоне не указано откуда брать данные - проверяем текстовые ноды
 		if(attrTo === "innerHTML" && attrTo == attrFrom) {
-			var forseReturn = false;
+			var textNodesValues = [], forseReturn;
+			
+			//Сначало проверим, а не являются ли текущие текстовые ноды шаблоном
 			if(itemprop && itemprop != "")$A(element.childNodes).forEach(function(textChild) {//перебираем текстовые ноды
 				if(textChild.nodeType === 3) {
 					if(~textChild.nodeValue.indexOf("#" + itemprop + "#")) {
@@ -46,105 +65,163 @@ function microdataItem_to_template(microdataItem, data) {
 				}
 			});
 			
+			if(forseReturn)return value;
+			
+			//Шаблоны не нашли - берём сохранённый шаблон
+			if(itemprop && itemprop != "" && templateElement)$A(templateElement.childNodes).forEach(function(textChild, i) {//перебираем текстовые ноды
+				if(textChild.nodeType === 3) {
+					if(~textChild.nodeValue.indexOf("#" + itemprop + "#")) {
+						textNodesValues.push(textChild.nodeValue.replace("#" + itemprop + "#", value));
+					}
+					else if(forse) {
+						textNodesValues.push(textChild.nodeValue);
+					}
+				}
+			});
+			
 			//Если не нашли ни одной текстовой ноды с "#itemprop#" - действуем стандартно
-			if(!forseReturn)element[attrTo] = value;
+			if(!textNodesValues.length)element[attrTo] = value;
+			else {
+				//Если мы в первый раз шаблонизируем элемент - сохраним первоначальный шаблон
+				if(!templateElement) {
+					element.__templateElement__ = _documentFragment.appendChild(cloneElement(element));
+				}
+				
+				$A(element.childNodes).forEach(function(textChild) {
+					if(textChild.nodeType === 3 && textNodesValues.length) {
+						textChild.nodeValue = textNodesValues.shift();
+					}
+				})
+				textNodesValues.forEach(function(new_textChild_value) {
+					element.appent(document.createTextNode(new_textChild_value));
+				})
+			}
 		}
 		else {
-			if(itemprop && ~element.getAttribute(attrFrom).indexOf("#" + itemprop + "#"))
+			if(itemprop && ~(element.getAttribute(attrFrom)||"").indexOf("#" + itemprop + "#"))
 				element[attrTo] = element.getAttribute(attrFrom).replace("#" + itemprop + "#", value);
 			else element[attrTo] = value;
 		}
-	}
-	
-	/** Обработать одно property
-	 *@param {string} */
-	function processOneProp(itemprop) {
-		var /** @type {List.<string>}*/
-			props = itemprop.split("."),
-			/** @type {Node} Временный элемент*/
-			tmplNode,
-			/** @type {Node} Текущий выбранный элемент*/
-			currentNode,
-			/** @type {Object} Настройки шаблона */
-			tmplOptions;
 		
-		$A(microdataItem.properties[itemprop] || []).forEach(function(currentNode) {	
-			var /** @type {number} */
-				i = 0,
-				/** @type {string} */
-				curProp_value = data,
-				/** @type {Boolean} Значение свойства - массив? */
-				curProp_value_isArray = false;
-				
-			tmplOptions = JSON.parse(currentNode.getAttribute("data-tmpl-options")) || {};
-			//Если в настройках переназначается itemprop
-			if(tmplOptions["itemprop"])itemprop = tmplOptions["itemprop"];
-			//Проверим, не хотим ли мы принудительно привести к массиву
-			if(itemprop.substr(-2) === "[]")curProp_value_isArray = true, itemprop = itemprop.substring(0, itemprop.length - 2);
-			
-			props = itemprop.split(".");
-			
-			while((curProp = props[i++]) && curProp_value) {
-				curProp_value = curProp_value[curProp];
-			}
-			//Проверим еще раз свойство на массив
-			//Если значение свойства - не массив, а в шаблоне указано, что массив - принудительно приводим значение сво-ва к массиву
-			curProp_value_isArray = curProp_value_isArray || Array.isArray(curProp_value);
-			
-			//Проверим, что мы получили последнее свойство в цепочке (prop1.prop2.prop3... etc)
-			if(i != props.length + 1)return;
-			
-			//Текущий элемент - Microdata Item
-			if(currentNode.getAttribute("itemscope") != null) {
-				// --- TEMP ---
-				// DOTO:: Заменить на первоначальное создание из microdataItem клона (cloneNode) в скрытом элементе или DocumentFragment как шаблона и получение этого шаблона для шаблонизации
-				/*var ttt = currentNode.getAttribute("itemtype") + randomString(5),
-					itemProp = currentNode.getAttribute("itemprop");
-				currentNode.setAttribute("itemtype", ttt);
-				currentNode.itemType = ttt;
-				currentNode.removeAttribute("itemprop");
-				currentNode = document.getItems(ttt)[0]
-				currentNode.setAttribute("itemprop", itemProp);
-				// --- END TEMP ---*/
-				
-				microdataItem_to_template(currentNode, curProp_value);
-			}
-			else {
-				if(curProp_value_isArray) {
-					$A(curProp_value).forEach(function(curVal) {//Принудительно преведём к массиву и пробежимся по нему
-						currentNode._lastInsertedNode = 
-							insertAfter(
-								currentNode.parentNode, 
-								cloneElement(currentNode),
-								currentNode._lastInsertedNode || currentNode);
-						setItemValue(currentNode._lastInsertedNode, curVal, itemprop);
-					})
-				}
-				else setItemValue(currentNode, curProp_value, itemprop);
-			}
-		})
+		return value;
 	}
 
-	if(Array.isArray(data) || (microdataItem.getAttribute("itemprop") || "").substr(-2) === "[]") {
-		$A(data).forEach(function(curData) {//Принудительно преведём к массиву и пробежимся по нему
-			microdataItem._lastInsertedNode = 
-				insertAfter(
-					microdataItem.parentNode, 
-					cloneElement(microdataItem),
-					microdataItem._lastInsertedNode || microdataItem);
-			microdataItem_to_template(microdataItem._lastInsertedNode, curData);
+/* PUBLICK */
+	
+	/** Установим значение одного property
+	 * @param {string} */
+	thisObj.setProperty = function(element, propertyName, data, forse) {
+		var /** @type {List.<string>}*/
+			props = propertyName.split("."),
+			/** @type {number} */
+			i = 1,
+			/** @type {Object} Настройки шаблона */
+			tmplOptions,
+			/** @type {string} */
+			curProp_value = data,
+			/** @type {Boolean} Значение свойства - массив? */
+			curProp_value_isArray = false;
+			
+		tmplOptions = JSON.parse(element.getAttribute("data-tmpl-options")) || {};
+		//Если в настройках переназначается propertyName
+		if(tmplOptions["itemprop"])propertyName = tmplOptions["itemprop"];
+		//Проверим, не хотим ли мы принудительно привести к массиву
+		if(propertyName.substr(-2) === "[]")curProp_value_isArray = true, propertyName = propertyName.substring(0, propertyName.length - 2);
+		
+		props = propertyName.split(".");
+		
+		while((curProp = props[i++]) && curProp_value) {
+			curProp_value = curProp_value[curProp];
+		}
+		//Проверим еще раз свойство на массив
+		//Если значение свойства - не массив, а в шаблоне указано, что массив - принудительно приводим значение сво-ва к массиву
+		curProp_value_isArray = curProp_value_isArray || Array.isArray(curProp_value);
+		
+		//Проверим, что мы получили последнее свойство в цепочке (prop1.prop2.prop3... etc)
+		if(curProp_value === void 0 || curProp_value === null || i != props.length + 1)return;
+		
+		//Текущий элемент - Microdata Item
+		if(element.getAttribute("itemscope") != null) {
+			thisObj.setItem(element, data, forse);
+		}
+		else {
+			if(curProp_value_isArray) {
+				var first = true, el;
+				$A(curProp_value).forEach(function(curVal) {//Принудительно преведём к массиву и пробежимся по нему
+					if(first) {
+						setItemValue(element, curVal, propertyName, forse);
+						
+						first = false;
+						return;
+					}
+					
+					el = insertAfter(
+						element.parentNode, 
+						cloneElement(element),
+						element._lastInsertedNode || element);
+						
+					el.__templateElement__ = element.__templateElement__;
+					setItemValue(el, curVal, propertyName, forse);
+				})
+			}
+			else setItemValue(element, curProp_value, propertyName, forse);
+		}
+	}
+	
+	thisObj.setItem = function(itemElement, data, forse) {
+		
+		if(Array.isArray(data) || (itemElement.getAttribute("itemprop") || "").substr(-2) === "[]") {
+			var first = true, el;
+			$A(data).forEach(function(curData) {//Принудительно преведём к массиву и пробежимся по нему
+				if(first) {
+					itemElement.__templateElement__ = _documentFragment.appendChild(cloneElement(itemElement));
+					thisObj.setItem(itemElement, curData, forse);
+					
+					first = false;
+					return;
+				}
+				el = insertAfter(
+					itemElement.parentNode, 
+					cloneElement(itemElement.__templateElement__),
+					itemElement._lastInsertedNode || itemElement);
+					
+				el.__templateElement__ = itemElement.__templateElement__;
+				thisObj.setItem(el, curData, forse);
+			})
+			return;
+		}
+
+		//Для старых браузеров: пофиксим Microdata-элемент
+		itemElement = global.MicrodataJS.fixItemElement(itemElement);
+			
+		$A(itemElement.properties.names).forEach(function(name) {
+			$A(itemElement.properties[name]).forEach(function(DOMElement) {
+				if(DOMElement.getAttribute("itemscope") != null && data[name]) {
+					thisObj.setItem(DOMElement, data[name], forse);
+				}
+				else {
+					thisObj.setProperty(DOMElement, name, data[name.split(".")[0]], forse);
+				}
+			})
 		})
 	}
-	else {
-		//Для старых браузеров: пофиксим Microdata-элемент
-		microdataItem = global.MicrodataJS.fixItemElement(microdataItem);
+	
+	thisObj.set = function(item_or_property) {
+	
+	}
+	
+/* INIT */
+	thisObj.init = function() {
 		
-		$A(microdataItem.properties.names).forEach(processOneProp);
 	}
 }
 
+})(window, MicrodataJS);
+
+
+
 //TEST
-(function fixPrototypes(global) {
+/*(function fixPrototypes(global) {
 	if(fixPrototypes.fixed)return;
 
 	function $addEventListener(elt, event, listener) {
@@ -162,4 +239,4 @@ function microdataItem_to_template(microdataItem, data) {
 		PropertyNodeList.prototype.test = "123"
 		fixPrototypes.fixed = true;
 	}
-})(window)
+})(window)*/
