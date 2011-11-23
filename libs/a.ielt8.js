4819f2273e058db3c546619673df63538047c7d1
Egor Nikolaev <termi_uc@inbox.ru>
2011-11-23 13:11:56 +0400

    * DOM-shim fix in a.js

--- libs/a.ielt8.js
+++ libs/a.ielt8.js
@@ -9,6 +9,26 @@ window.browser && window.browser.msie && (ie = window.browser.msie);
 
 if(ie < 8) {
 
+// Node.attributes path for IE < 8
+// No Node.attributes patch for document.head :(
+window["_ielt8_getAttributes"] = function __getAtt() {
+	var tmp = this._ && this._["__ielt8_attributes__"],
+		res = {length : 0},
+		val;
+	
+	if(!tmp)throw Error("__ielt8_attributes__ is requared")
+	
+	for(var i = 0, l = tmp.length, k = 0 ; i < l ; i++)if((val = tmp[i]).specified && !(val.name in __getAtt.notAnAttribute)){
+		res[k++] = val;
+		res[tmp[i].name] = val;
+		res.length++;
+	}
+	return res;
+}
+__getAtt.notAnAttribute = {"insertAfter" : 1, "getElementsByClassName" : 1, "compareDocumentPosition" : 1, "_" : 1, "getAttribute" : 1, "setAttribute" : 1, "addEventListener" : 1, "removeEventListener" : 1, "dispatchEvent" : 1}
+//
+
+
 var __URL_TO_ELEMENT_BEHAVIOR__='a.ielt8.htc',
 	__URL_TO_IE6_ELEMENT_BEHAVIOR__='a.ie6.htc',
 	__STYLE_ID="ielt8_style_prev_for_behaviour";
