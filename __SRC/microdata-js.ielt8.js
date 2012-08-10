54af0b01a13eec4ba6e966a1d1289325926ad7cc
termi <termi_uc@inbox.ru>
2012-08-10 14:47:37 +0400

    minor fixes

--- __SRC/microdata-js.ielt8.js
+++ __SRC/microdata-js.ielt8.js
@@ -1,28 +1,46 @@
 // ==ClosureCompiler==
-// @compilation_level SIMPLE_OPTIMIZATIONS
+// @compilation_level ADVANCED_OPTIMIZATIONS
 // @output_file_name microdata-js.ielt8.js
 // ==/ClosureCompiler==
 
 // This file MUST be in <head> section of document
-// required window._ielt8_Element_proto
+// required
+// 1. window.Node?prototype?ielt8
+// 2. document.head
 
 ;(function() {
 
-//[bugfix] due to https://github.com/h5bp/html5-boilerplate/issues/378 update IE detection
-if(window.Node.prototype["ielt8"]) {//IE < 8 polifill
+var __URL_TO_ELEMENT_BEHAVIOR__
+  , __STYLE_ID
 
-var __URL_TO_ELEMENT_BEHAVIOR__='microdata-js.ielt8.htc',
+  , prevStyle
+  , add
+  , tmp
+;
+
+//[bugfix] due to https://github.com/h5bp/html5-boilerplate/issues/378 update IE detection
+if((tmp = window["Node"]) && (tmp = tmp.prototype) && tmp["ielt8"]) {//IE < 8 polifill
+
+	__URL_TO_ELEMENT_BEHAVIOR__ = 'microdata-js.ielt8.htc';
 	__STYLE_ID="ielt8_style_prev_for_behaviour";
 
-var prevStyle=document.getElementById(__STYLE_ID),add="";
-if(prevStyle){
-add=prevStyle.getAttribute("data-url")||"";
-prevStyle.id="";
-}
+	prevStyle = document.getElementById(__STYLE_ID);
+	add = "";
 
-add+=" url(\""+__URL_TO_ELEMENT_BEHAVIOR__+"\") ";
+	if(prevStyle){
+		add = prevStyle.getAttribute("data-url") || "";
+		prevStyle.id = "";
+	}
 
-document.write("<style id='"+__STYLE_ID+"' data-url='"+add+"'>html,body,div,span,object,iframe,h1,h2,h3,h4,h5,h6,p,blockquote,pre,abbr,address,cite,code,del,dfn,em,img,ins,kbd,q,samp,small,strong,sub,sup,var,b,i,dl,dt,dd,ol,ul,li,fieldset,form,label,legend,table,caption,tbody,tfoot,thead,tr,th,td,article,aside,canvas,details,figcaption,figure,footer,header,hgroup,menu,nav,section,summary,time,mark,audio,video,textarea{behavior: "+add+"}</style>");
+	add += " url(\""+__URL_TO_ELEMENT_BEHAVIOR__+"\") ";
+
+	tmp = "<style id='" + __STYLE_ID + "' data-url='" + add + "'>*{behavior: " + add + "}</style>";
+
+	document.readyState == "complete" ? 
+		document.head.insertAdjacentHTML("beforeend", "<br>" + tmp)	
+		:
+		document.write(tmp)
+	;
 
 }
 
